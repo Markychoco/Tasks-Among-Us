8e5d0fc2085a9b5077acaece5101055f001bc54b
Michael Diggs <mjdiggs18.e@gmail.com>
2020-10-23 14:56:18 -0400

    Finished Mira HQ tasks.

--- javascript/mirahq.js
+++ javascript/mirahq.js
@@ -4,22 +4,10 @@ const cafeteria = document.querySelector('.cafeteria-task');
 const cafeteriaTask = document.querySelector('.cafeteria-task-shown');
 const communications = document.querySelector('.communications-task');
 const communicationTask = document.querySelector('.communications-task-shown');
-const electrical = document.querySelector('.electrical-task');
-const electricalTask = document.querySelector('.electrical-task-shown');
-const engines = document.querySelector('.engines-task');
-const enginesTask = document.querySelector('.engines-task-shown');
 const medbay = document.querySelector('.medbay-task');
 const medbayTask = document.querySelector('.medbay-task-shown');
-const navigation = document.querySelector('.navigation-task');
-const navigationTask = document.querySelector('.navigation-task-shown');
-const o2 = document.querySelector('.o2-task');
-const o2Task = document.querySelector('.o2-task-shown');
 const reactor = document.querySelector('.reactor-task');
 const reactorTask = document.querySelector('.reactor-task-shown');
-const security = document.querySelector('.security-task');
-const securityTask = document.querySelector('.security-task-shown');
-const shields = document.querySelector('.shields-task');
-const shieldsTask = document.querySelector('.shields-task-shown');
 const storage = document.querySelector('.storage-task');
 const storageTask = document.querySelector('.storage-task-shown');
 const weapons = document.querySelector('.weapons-task');
@@ -169,14 +157,8 @@ const opened = document.querySelector('.tablet-tasks');
 const exitTask = document.querySelector('.exit-task');
 const exitTaskCafe = document.querySelector('.exit-task-cafe');
 const exitTaskComms = document.querySelector('.exit-task-comms');
-const exitTaskElec = document.querySelector('.exit-task-elec');
-const exitTaskEng = document.querySelector('.exit-task-eng');
 const exitTaskMed = document.querySelector('.exit-task-med');
-const exitTaskNav = document.querySelector('.exit-task-nav');
-const exitTaskO2 = document.querySelector('.exit-task-o2');
 const exitTaskRec = document.querySelector('.exit-task-rec');
-const exitTaskSec = document.querySelector('.exit-task-sec');
-const exitTaskShield = document.querySelector('.exit-task-shield');
 const exitTaskStor = document.querySelector('.exit-task-stor');
 const exitTaskWeps = document.querySelector('.exit-task-weps');
 const returnMobile = document.querySelector('.returnMobile');
@@ -228,57 +210,9 @@ exitTaskComms.addEventListener('click', () => {
     }
 });
 
-exitTaskElec.addEventListener('click', () => {
-    opened.style.display = 'flex';
-    electricalTask.style.display = 'none';
-    returnMobile.style.display = 'block';
-
-    if (window.innerWidth <= 1085) {
-        returnMobile.style.display = 'block';
-    } else {
-        returnMobile.style.display = 'none';
-    }
-});
-
-exitTaskEng.addEventListener('click', () => {
-    opened.style.display = 'flex';
-    enginesTask.style.display = 'none';
-    returnMobile.style.display = 'block';
-
-    if (window.innerWidth <= 1085) {
-        returnMobile.style.display = 'block';
-    } else {
-        returnMobile.style.display = 'none';
-    }
-});
-
 exitTaskMed.addEventListener('click', () => {
     opened.style.display = 'flex';
     medbayTask.style.display = 'none';
-    returnMobile.style.display = 'block';
-
-    if (window.innerWidth <= 1085) {
-        returnMobile.style.display = 'block';
-    } else {
-        returnMobile.style.display = 'none';
-    }
-});
-
-exitTaskNav.addEventListener('click', () => {
-    opened.style.display = 'flex';
-    navigationTask.style.display = 'none';
-    returnMobile.style.display = 'block';
-
-    if (window.innerWidth <= 1085) {
-        returnMobile.style.display = 'block';
-    } else {
-        returnMobile.style.display = 'none';
-    }
-});
-
-exitTaskO2.addEventListener('click', () => {
-    opened.style.display = 'flex';
-    o2Task.style.display = 'none';
     returnMobile.style.display = 'block';
 
     if (window.innerWidth <= 1085) {
@@ -300,29 +234,6 @@ exitTaskRec.addEventListener('click', () => {
     }
 });
 
-exitTaskSec.addEventListener('click', () => {
-    opened.style.display = 'flex';
-    securityTask.style.display = 'none';
-    returnMobile.style.display = 'block';
-
-    if (window.innerWidth <= 1085) {
-        returnMobile.style.display = 'block';
-    } else {
-        returnMobile.style.display = 'none';
-    }
-});
-
-exitTaskShield.addEventListener('click', () => {
-    opened.style.display = 'flex';
-    shieldsTask.style.display = 'none';
-    returnMobile.style.display = 'block';
-
-    if (window.innerWidth <= 1085) {
-        returnMobile.style.display = 'block';
-    } else {
-        returnMobile.style.display = 'none';
-    }
-});
 
 exitTaskStor.addEventListener('click', () => {
     opened.style.display = 'flex';
@@ -335,19 +246,6 @@ exitTaskStor.addEventListener('click', () => {
         returnMobile.style.display = 'none';
     }
 });
-
-exitTaskWeps.addEventListener('click', () => {
-    opened.style.display = 'flex';
-    weaponsTask.style.display = 'none';
-    returnMobile.style.display = 'block';
-
-    if (window.innerWidth <= 1085) {
-        returnMobile.style.display = 'block';
-    } else {
-        returnMobile.style.display = 'none';
-    }
-});
-
 
 
 //Task Open Event Listners
@@ -370,33 +268,10 @@ communications.addEventListener('click', () => {
     returnMobile.style.display = 'none';
 });
 
-electrical.addEventListener('click', () => {
-    opened.style.display = 'none';
-    electricalTask.style.display = 'block';
-    returnMobile.style.display = 'none';
-});
-
-engines.addEventListener('click', () => {
-    opened.style.display = 'none';
-    enginesTask.style.display = 'block';
-    returnMobile.style.display = 'none';
-});
 
 medbay.addEventListener('click', () => {
     opened.style.display = 'none';
     medbayTask.style.display = 'block';
-    returnMobile.style.display = 'none';
-});
-
-navigation.addEventListener('click', () => {
-    opened.style.display = 'none';
-    navigationTask.style.display = 'block';
-    returnMobile.style.display = 'none';
-});
-
-o2.addEventListener('click', () => {
-    opened.style.display = 'none';
-    o2Task.style.display = 'block';
     returnMobile.style.display = 'none';
 });
 
@@ -406,26 +281,8 @@ reactor.addEventListener('click', () => {
     returnMobile.style.display = 'none';
 });
 
-security.addEventListener('click', () => {
-    opened.style.display = 'none';
-    securityTask.style.display = 'block';
-    returnMobile.style.display = 'none';
-});
-
-shields.addEventListener('click', () => {
-    opened.style.display = 'none';
-    shieldsTask.style.display = 'block';
-    returnMobile.style.display = 'none';
-});
-
 storage.addEventListener('click', () => {
     opened.style.display = 'none';
     storageTask.style.display = 'block';
-    returnMobile.style.display = 'none';
-});
-
-weapons.addEventListener('click', () => {
-    opened.style.display = 'none';
-    weaponsTask.style.display = 'block';
     returnMobile.style.display = 'none';
 });
